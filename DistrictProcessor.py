23012fa8bb6e68440d333e3ed894542b8b104ae6
tamtzehei <tamtzehei@users.noreply.github.com>
2018-11-08 13:46:22 -0500

    Added test data sheet

---
+++ DistrictProcessor.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 23:39:14 2018
+
+@author: Tze Hei
+"""
+
+class District:
+    
+    
+    def __init__(self,):
---
+++ constants.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 00:09:02 2018
+
+@author: Tze Hei
+"""
+
+SIZE_OF_DISTRICT = 9.0
+ROWS_OF_MAP = 6
+COLS_OF_MAP = 6
+ROWS_OF_DISTRICT = 6
+START_COL_OF_DISTRICT = 6
+NUM_OF_DISTRICTS = 4
